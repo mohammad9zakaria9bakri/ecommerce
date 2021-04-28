b5c96270d16970f37aec9102eeebf82f167dd030
mohammad9zakaria9bakre <zakryuybakre@gmail.com>
2021-04-28 11:54:00 -0700

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,7 @@ module.exports = {
                         }
                     },
                     'css-loader',
+                    'sass-loader'
 
                 ]
             },
@@ -91,6 +92,33 @@ module.exports = {
             template: "./src/index.html",
 
         }),
+        new HtmlWebpackPlugin({
+            filename: "product.html",
+            template: "./src/product.html",
+
+        }),
+        new HtmlWebpackPlugin({
+            filename: "checkout.html",
+            template: "./src/checkout.html",
+
+        }),
+        new HtmlWebpackPlugin({
+            filename: "payment.html",
+            template: "./src/payment.html",
+
+        }),
+        new HtmlWebpackPlugin({
+            filename: "search.html",
+            template: "./src/search.html",
+
+        }),
+        new HtmlWebpackPlugin({
+            filename: "contact.html",
+            template: "./src/contact.html",
+
+        }),
+
+
         new MiniCssExtractPlugin({ filename: "css/style.css" }),
         new OptimizeCSSAssetsPlugin({}),
 
